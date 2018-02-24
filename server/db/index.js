640a5f5a5a125735970cf61750e24c37f0588981
Carter Duncan <dukeblue2017@gmail.com>
2018-02-24 11:14:15 -0600

    Add ESLint to whole project

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars, no-console */
 const dotenv = require('dotenv').config({
-  path: 'env.env'
+  path: 'env.env',
 });
+
 const knex = require('knex')({
   client: 'mysql',
   connection: {
@@ -8,11 +10,11 @@ const knex = require('knex')({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
-  }
-})
+  },
+});
 
-knex('users').then( res => console.log('knex res', res)).catch( err => console.log('knex err:', err))
+knex('users').then(res => console.log('knex res', res)).catch(err => console.log('knex err:', err));
 
 const db = {};
 
-module.exports = db
+module.exports = db;
