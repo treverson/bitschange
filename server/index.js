53166e428a3e0058c8ba088c5736334c6e88de0b
Carter Duncan <dukeblue2017@gmail.com>
2018-02-23 18:09:43 -0600

    Perform initial setup with Create React App
    Also set up root directory, with client directory,
    server directory, .gitignore, and NPM.

---
+++ server/index.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const path = require('path');
+// const port = 3000;
+// const app = express();
+
+// app.use(express.static(__dirname + '/../client/public'))
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`)
+// });
