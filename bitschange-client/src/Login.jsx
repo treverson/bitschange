ace231379438bf191af66516d759ffde9e239c2c
Carter Duncan <dukeblue2017@gmail.com>
2018-02-24 11:59:10 -0600

    Set up login on client

--- bitschange-client/src/Login.jsx
+++ bitschange-client/src/Login.jsx
@@ -1,16 +1,50 @@
+/* eslint-disable max-len */
 import React, { Component } from 'react';
 
 class LoginPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUsernameChange(event) {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({
+      password: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    console.log('submitted', this.state)
+    event.preventDefault();
   }
 
   render() {
     return (
       <div className="login-page">
         <div className="login-box">
-          <div>Username:<input /></div>
-          <div>Password<input /></div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Username:
+              <input type="text" value={this.state.username} onChange={this.handleUsernameChange} />
+            </label>
+            <label>
+              Password:
+              <input type="text" value={this.state.password} onChange={this.handlePasswordChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
         </div>
       </div>
     );
